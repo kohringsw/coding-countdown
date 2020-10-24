fa8fcbfe71f214ede91bbb6014df21582a05556a
MyBoyDenis <shelbykohring@Shelbys-MacBook-Pro.local>
2020-10-23 23:07:44 -0500

    added quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+var buttonEl = document.querySelector("#start-button");
+var startQuestionsEl = document.querySelector("#questions");
+
+buttonEl.addEventListener("click", function() {
+    
+})
+
+// questions for the Coding Countdown Challenge
+var questions = [
+    {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
+    correct: "C. Alerts"
+    },
+    
+    {
+    question: "The condition in an if/else statement is enclosed within:",
+    choices: ["A. Parentheses", "B. Curly brackets", "C. Quotes", "D. Square brackets"],
+    correct: "A. Parentheses"
+    },
+
+    {
+    question: "Arrays in JavaScript can be used to store:",
+    choices: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
+    correct: "D. All of the above"
+    },
+
+    {
+    question: "When being assigned to variables, strings must be enclosed within:",
+    choices: ["A. Commas", "B. Quotes", "C. Curly Brackets", "D. Parentheses"],
+    correct: "B. Quotes"
+    },
+
+    {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["A. Console.log", "B. GitHub", "C. JavaScript", "D. For loops"],
+    correct: "A. Console.log"
+    },
+]   
