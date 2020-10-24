f7361ef3d03759be85f5b4aaf269b1657a447f82
MyBoyDenis <shelbykohring@Shelbys-MacBook-Pro.local>
2020-10-24 09:56:38 -0500

    committing for review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,11 @@
-var buttonEl = document.querySelector("#start-button");
-var startQuestionsEl = document.querySelector("#questions");
+var startButton = document.getElementById("start-button");
 
-buttonEl.addEventListener("click", function() {
-    
-})
+// show questions and hide introductory page
+function showQuestions() {
+    var showQuestionsEl = document.getElementById("#showQuestions");
+    showQuestionsEl.style.display = "flex";
+    var introEl = document.getElementById("#intro");
+    introEl.style.display = "none";
+}
 
-// questions for the Coding Countdown Challenge
-var questions = [
-    {
-    question: "Commonly used data types DO NOT include:",
-    choices: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
-    correct: "C. Alerts"
-    },
-    
-    {
-    question: "The condition in an if/else statement is enclosed within:",
-    choices: ["A. Parentheses", "B. Curly brackets", "C. Quotes", "D. Square brackets"],
-    correct: "A. Parentheses"
-    },
-
-    {
-    question: "Arrays in JavaScript can be used to store:",
-    choices: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
-    correct: "D. All of the above"
-    },
-
-    {
-    question: "When being assigned to variables, strings must be enclosed within:",
-    choices: ["A. Commas", "B. Quotes", "C. Curly Brackets", "D. Parentheses"],
-    correct: "B. Quotes"
-    },
-
-    {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["A. Console.log", "B. GitHub", "C. JavaScript", "D. For loops"],
-    correct: "A. Console.log"
-    },
-]   
+startButton.onclick = showQuestions;
